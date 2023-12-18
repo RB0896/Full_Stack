e3cf5046df2bdc6af1e828cde57f5863cf6650b0
RB <131417040+RB0896@users.noreply.github.com>
2023-12-18 11:19:19 -0500

    Add files via upload
    Assignment_1

---
+++ Variables_Example1.java
@@ -0,0 +1,25 @@
+class Variables_Example1
+{
+	public static void main(String[] args)
+	{
+		int nummber1 =12;
+		int number2=13;
+		int number3=11;
+		int result;
+		int average;
+		int sub_result;
+		int mul_result;
+
+		result =nummber1+number2+number3;
+		average=result/3;
+		sub_result=nummber1-number2-number3;
+		mul_result=nummber1*number2*number3;
+
+		System.out.println("Sum of the three numbers is : "+result);
+		System.out.println("average of three numbers is "+average);
+		System.out.println("sub_result of three numbers is "+sub_result);
+		System.out.println("mul_result of three numbers is "+mul_result);
+
+
+	}
+}
