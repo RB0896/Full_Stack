0bf06d71a724c4cea1e25d243ac938c0bbbd68be
RB <131417040+RB0896@users.noreply.github.com>
2024-01-29 17:42:21 -0500

    String_Assignment

---
+++ ConvertL.java
@@ -0,0 +1,10 @@
+package stringassignment;
+
+public class ConvertL {
+
+	public static void main(String[] args) {
+		String s="bvrit college";
+		System.out.println(s.lastIndexOf('l'));
+	}
+
+}
---
+++ ConvertRaju.java
@@ -0,0 +1,14 @@
+package stringassignment;
+
+public class ConvertRaju {
+
+	public static void main(String[] args) {
+		String s = "b.v.raju college";
+		
+
+		
+		System.out.println(s.substring(4, 8).toUpperCase());
+
+	}
+
+}
